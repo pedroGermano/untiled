b395f1ac6f0fea3c8016a932013c3f92bd005ba2
Pedro Germano <pedrogermano232@gmail.com>
2024-09-26 10:42:48 -0300

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
-import { Header } from "./components/Header";
 import { SectionCompanies } from "./components/SectionCompanies";
 import { SectionHero } from "./components/SectionHero";
 import { SectionFeatures } from "./components/SectionFeatures";
 import { SectionTestimonials } from "./components/SectionTestimonials";
 import { SectionAnalitycs } from "./components/SectionAnalitycs";
 import { SectionQuestions } from "./components/SectionQuestions";
+import { SectionLauch } from "./components/SectionLauch";
+import { SectionTrial } from "./components/SectionTrial";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <SectionHero />
       <SectionCompanies />
       <SectionFeatures />
       <SectionTestimonials />
       <SectionAnalitycs />
       <SectionQuestions />
+      <SectionLauch />
+      <SectionTrial />
     </>
   );
 }
